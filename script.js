8d4fb68fa280dea0e2fcfbeae752cb3100aca1f0
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-12-01 15:06:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let obj={
+	name:"Pranay"
+}
+Object.prototype.getKeys=()=>{
+	let arr=Object.getOwnPropertyNames(obj);
+	return arr;
+}
+console.log(obj.getKeys())
